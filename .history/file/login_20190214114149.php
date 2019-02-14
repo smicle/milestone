21d044dbf073115fc42242a4928323ab330afc3c
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2019-02-14 11:47:55 +0900

    account

---
+++ .history/file/login_20190214114149.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/default.css" type="text/css">
+	<title>ログイン</title>
+</head>
+<body>
+<div id="input">
+<h1>ログイン</h1>
+<span class="error"></span>
+<form method="post" action="login_chk.php">
+<table>
+<tr><th>ユーザID:</th><td colspan="2"><input type="text" name="id" size="12"></td></tr>
+<tr><th>パスワード:</th><td><input type="password" name="password" size="20"></td><td><input id="submit_button" type="submit" value="ログイン"></td></tr>
+</table>
+</form>
+</div>
+</body>
+</html>
